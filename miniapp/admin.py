a00eb45d2b774871cf70c13d980a73e1df144ecb
SpaceTurtle-git <rockfist1436@gmail.com>
2025-04-15 08:58:14 +0530

    hi
    hi

--- miniapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.\
-from miniapp.models import Movie, Video, Profile, CustomUser
-
-admin.site.register(Movie)
-admin.site.register(Video)
-admin.site.register(Profile)
-admin.site.register(CustomUser)
--- miniapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Home
-
-app_name = "miniapp"
-
-urlpatterns = [
-    path("", Home.as_view(), name="home"),
-]
